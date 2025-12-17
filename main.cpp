6f07ee7e9028dedc421587e1ee0a71d89bbe2c0f
Anatoliy <and1life@yandex.ru>
2025-12-18 01:56:08 +0300

    feat(ImageFileOutput): added implementation ImageFileOutput()

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "core/Image.hpp"
 #include "io/ConsoleOutput.hpp"
 #include "core/ImageConverter.hpp"
+#include "io/ImageFileOutput.hpp"
 
 int main(int argc, char const *argv[])
 {
@@ -11,8 +12,12 @@ int main(int argc, char const *argv[])
 
         ImageConverter::convertToGrayscale(image);
 
-        ConsoleOutput consoleOutput;
-        consoleOutput.output(image);
+        // ConsoleOutput consoleOutput;
+        // consoleOutput.output(image);
+
+        ImageFileOutput fileOutput("output.jpg");
+        fileOutput.output(image);
+        
     }
     catch(const std::exception& e)
     {
--- src/io/ImageFileOutput.cpp
+++ src/io/ImageFileOutput.cpp
@@ -1,3 +1,48 @@
 #include "io/ImageFileOutput.hpp"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
+#include <vector>
+
 ImageFileOutput::ImageFileOutput(const std::string &filepath) : filepath(filepath) {}
+
+void ImageFileOutput::output(const Image &image) const
+{
+    std::vector<unsigned char> data;
+    data.reserve(image.getWidth() * image.getHeight() * image.getChannels());
+
+    for (size_t y = 0; y < image.getHeight(); ++y)
+    {
+        for (size_t x = 0; x < image.getWidth(); ++x)
+        {
+            for (size_t c = 0; c < image.getChannels(); ++c)
+            {
+                data.push_back(static_cast<unsigned char>(image.getPixel(x, y, c)));
+            } 
+        }   
+    }
+    
+    size_t dotPos = filepath.find_last_of('.');
+    std::string ext = (dotPos != std::string::npos) ? filepath.substr(dotPos) : "";
+
+    if (ext == ".png")
+    {
+        stbi_write_png(filepath.c_str(), image.getWidth(), image.getHeight(),
+         image.getChannels(),data.data(), image.getWidth() * image.getChannels());
+    } else if (ext == ".jpg" || ext == ".jpeg")
+    {
+        stbi_write_jpg(filepath.c_str(), image.getWidth(), image.getHeight(),
+         image.getChannels(), data.data(), 100);
+    } else if (ext == ".bmp")
+    {
+        stbi_write_bmp(filepath.c_str(), image.getWidth(), image.getHeight(),
+         image.getChannels(), data.data());
+    } else 
+    {
+        throw std::runtime_error("Unsupported file format.");
+    }
+    
+    
+    
+}
